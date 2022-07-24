4b3d2b3e903879e25c7b9992da96dfa0d1c9299e
Shadab K <skarim@Shadabs-MacBook-Air.local>
2022-07-24 06:11:59 -0400

    LAB PART 2

--- characters/main_app/models.py
+++ characters/main_app/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+class Character(models.Model):
+    name = models.CharField(max_length=100)
+    media_name = models.CharField(max_length=100)
+    movie_format = models.BooleanField(default=False)
+    year_created = models.IntegerField()
+    description = models.TextField(max_length=500)
+    role = models.CharField(max_length=100)
+    goal = models.CharField(max_length=100)
+    obstacle = models.CharField(max_length=100)
+  
+def __str__(self):
+    return self.name
--- characters/main_app/views.py
+++ characters/main_app/views.py
@@ -1,29 +1,19 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
-class Character:
-  def __init__(self, name, media_name, movie_format, description, role, goal, obstacle):
-    self.name = name
-    self.media_name = media_name
-    self.movie_format = movie_format
-    self.description = description
-    self.role = role
-    self.goal = goal
-    self.obstacle = obstacle
-
-characters = [
-  Character("Rustin Cohle", "True Detective", False, "One of two detectives who hunts a serial killer in Louisiana for 17 years", "Protagonist", "Finding the killer", "Complex murder case"),
-  Character("Tony Soprano", "The Sopranos", False, "Mob boss Tony Soprano deals with personal and professional issues in his home and business life that affect his mental state.", "Main character", "Becoming a varsity athlete", "Crime life and family life."),
-  Character("Creed Bratton", "The Office", False, "An old clueless dude working at an office full of clashes, inappropriate behavior and tedium", "Supporting character", "Scuba", "Asthma")
-]
+from .models import Character
 
 # Create your views here.
 
 def home(request):
-  return HttpResponse('<h1>Hello /ᐠ｡‸｡ᐟ\ﾉ</h1>')
+  return render(request, "home.html")
 
 def about(request):
     return render(request, "about.html")
 
 def characters_index(request):
-    return render(request, "characters/index.html", { "characters": characters })
+  characters = Character.objects.all()
+  return render(request, "characters/index.html", { "characters": characters })
+
+def characters_detail(request, character_id):
+  character = Character.objects.get(id=character_id)
+  return render(request, "characters/detail.html", { "character": character })
